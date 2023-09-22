1b5146291a7c490ee20bbf53d5abcab68d231e5b
Sagar Parmar <Sagar Parmar>
2023-09-22 20:25:15 +0530

    Added code for SSH key and Volume

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
-# goCodeForAWS
+# Go Code For AWS Resources
 
-## This repo does not contains production ready code.
-## The repo contains code to create resource on AWS.
- I have created this repo for my learning. 
+# This repo does not contains production ready code.
+# The repo contains code to create resource on AWS.
+# I have created this repo for my learning. 
--- main.go
+++ main.go
@@ -36,23 +36,84 @@ func createEc2(ctx context.Context, region string, profile string) (string, erro
 
 	ec2Client := ec2.NewFromConfig(cfg)
 
-	key, err := ec2Client.CreateKeyPair(ctx, &ec2.CreateKeyPairInput{
-		KeyName: aws.String("sagar"),
-	})
-	if err != nil {
-		return "", fmt.Errorf("Create KeyPair error:  %s", err)
+	// Defined Tags
+	tags := []types.Tag{
+		{
+			Key:   aws.String("Name"),
+			Value: aws.String("Sagar"),
+		},
+		{
+			Key:   aws.String("ENV"),
+			Value: aws.String("Learning"),
+		},
+	}
+	tagSpecifications := []types.TagSpecification{
+		{
+			ResourceType: types.ResourceTypeInstance,
+			Tags:         tags,
+		},
 	}
 
-	err = os.WriteFile("sagar.pem", []byte(*key.KeyMaterial), 0600)
+	// Volume variables
+	volumeSize := int32(11) // Adjust the size as needed
+	volumeType := types.VolumeTypeGp2
+	availabilityZone := "us-east-1a"
+
+	// Describe KeyPair
+	sshKey, err := ec2Client.DescribeKeyPairs(ctx, &ec2.DescribeKeyPairsInput{
+		KeyNames: []string{"sagar"},
+	})
+
+	// Creating Key Pair.
 	if err != nil {
 		if awsErr, ok := err.(awserr.Error); ok {
 			if awsErr.Code() == "InvalidKeyPair.NotFound" {
-				return "", fmt.Errorf("error in creating key file, %s", awsErr)
+				// Key pair doesn't exist, create it
+				key, err := ec2Client.CreateKeyPair(ctx, &ec2.CreateKeyPairInput{
+					KeyName: aws.String("sagar"),
+				})
+				if err != nil {
+					return "", fmt.Errorf("Errors while creating :  %s", err)
+				}
+				err = os.WriteFile("sagar.pem", []byte(*key.KeyMaterial), 0600)
+				if err != nil {
+					return "", fmt.Errorf("Write KeyPair error: %s", err)
+				}
+			} else if awsErr.Code() == "InvalidKeyPair.Duplicate" {
+				// Handle other error cases
+				log.Println("Key pair already exist")
+			} else {
+				return "", fmt.Errorf("Some error has occured while creating key: %s", err)
 			}
 		}
-		return "", fmt.Errorf("Write KeyPair error:  %s", err)
+	}
+	if sshKey == nil {
+		sshKey, err := ec2Client.CreateKeyPair(ctx, &ec2.CreateKeyPairInput{
+			KeyName: aws.String("sagar"),
+		})
+		if err != nil {
+			return "", fmt.Errorf("Write sshKey error: %s", err)
+		}
+
+		if err = os.WriteFile("sagar.pem", []byte(*sshKey.KeyMaterial), 0600); err != nil {
+			return "", fmt.Errorf("Write KeyPair error:  %s", err)
+		}
 	}
 
+	// if len(keyPair.KeyPairs) == 0 {
+	// 	key, err := ec2Client.CreateKeyPair(ctx, &ec2.CreateKeyPairInput{
+	// 		KeyName: aws.String("sagar"),
+	// 	})
+	// 	if err != nil {
+	// 		return "", fmt.Errorf("Create KeyPair error:  %s", err)
+	// 	}
+	// 	file := os.WriteFile("sagar.pem", []byte(*key.KeyMaterial), 0600)
+	// 	if file != nil {
+	// 		return "", fmt.Errorf("Write KeyPair error:  %s", file)
+	// 	}
+	// }
+
+	// Selecting Image for the Instance
 	imageOutput, err := ec2Client.DescribeImages(ctx, &ec2.DescribeImagesInput{
 		Filters: []types.Filter{
 			{
@@ -74,13 +135,32 @@ func createEc2(ctx context.Context, region string, profile string) (string, erro
 		return "", fmt.Errorf("Image output is empty ")
 	}
 
+	//  EBS block size configuration and mapping.
+	blockDeviceMappings := []types.BlockDeviceMapping{
+		{
+			DeviceName: aws.String("/dev/sda1"),
+			Ebs: &types.EbsBlockDevice{
+				DeleteOnTermination: aws.Bool(true),
+				VolumeSize:          aws.Int32(volumeSize),
+				VolumeType:          volumeType,
+			},
+		},
+	}
+
+	// Selecting Instance type
 	instance, err := ec2Client.RunInstances(ctx, &ec2.RunInstancesInput{
 		ImageId:      imageOutput.Images[0].ImageId,
 		KeyName:      aws.String("sagar"),
 		MinCount:     aws.Int32(1),
 		MaxCount:     aws.Int32(1),
 		InstanceType: types.InstanceTypeT2Micro,
+		Placement: &types.Placement{
+			AvailabilityZone: aws.String(availabilityZone),
+		},
+		BlockDeviceMappings: blockDeviceMappings,
+		TagSpecifications:   tagSpecifications,
 	})
+
 	if err != nil {
 		return "", fmt.Errorf("Run instance error error:  %s", err)
 	}
